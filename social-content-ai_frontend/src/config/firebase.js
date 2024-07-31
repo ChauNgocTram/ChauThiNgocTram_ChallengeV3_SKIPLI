dd7f19531f5faab3085b5150487e3e2ca5c6d249
Ngoc Tram <tramngoc3402@gmail.com>
2024-07-31 18:28:40 +0700

    update env

--- social-content-ai_frontend/src/config/firebase.js
+++ social-content-ai_frontend/src/config/firebase.js
@@ -3,18 +3,17 @@ import { getDatabase } from 'firebase/database';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBCPhOFLS1a3JEuNjSY0zf9nngnkUz5Tg",
-  authDomain: "challengev3-chauthingoctram.firebaseapp.com",
-  databaseURL: "https://challengev3-chauthingoctram-default-rtdb.firebaseio.com",
-  projectId: "challengev3-chauthingoctram",
-  storageBucket: "challengev3-chauthingoctram.appspot.com",
-  messagingSenderId: "204766693887",
-  appId: "1:204766693887:web:5815444c7659f88b4affca",
-  measurementId: "G-X945R733C6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
 
 const database = getDatabase(app);
 const analytics = getAnalytics(app);
